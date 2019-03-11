acfd485c2e937d3965ab3f3ae2a53c021189f8cc
Sameer Naumani <snaumani97@gmail.com>
2019-03-11 13:58:25 -0400

    updated lab3

--- DiscreteSignals/Lab 3-Discrete Time Fourier Series/PartAB.m
+++ DiscreteSignals/Lab 3-Discrete Time Fourier Series/PartAB.m
@@ -2,39 +2,47 @@
 %Part A 
 % A.1 Find fundamental period No and fundamental frequency Omega of x[n]
 % N = 2 Omega = pi
+clc
 N_0 = 2;
-n = (0:N_0-1);
-Omega_0 = (2.*pi)/N_0;
-x = @(n)(4.*cos(2.4.*pi.*n) + 2.*sin(3.2.*pi.*n));
-for r = 0:N_0-1
-    X_r(r+1) = sum(x(n).*exp(-j.*r.*Omega_0.*n))./N_0;
+n = (0 : N_0-1);
+Om_0 = 2*pi/N_0;                                                        % fundamental frequency of x[n]
+x = 4*cos(2.4*pi*n)+2*sin(3.2*pi*n);                                    % definition of x[n]
+for r = 0: N_0-1,
+    X_r(r+1) = sum(x.* exp(-j.*r.*n.*Om_0))/N_0;                        % computing the DFTS of x[n]
 end
-r=n;
-X_r = fft(x(n))./N_0;
-stem(r,abs(X_r),'k.');
-xlabel('r'); 
-ylabel('X_r');
-figure
-stem(r,imag(X_r));
 
-%A.3
-N_1 = 6;
-n = (0:N_1-1);
-Omega_0 = (2.*pi)/N_1;
-x = [3,2,1,0,1,2];
-for r = 0:N_1-1
-    X_r(r+1) = sum(x.*exp(-j.*r.*Omega_0.*n))./N_1;
+figure
+subplot(2,1,1)
+stem(real(X_r))
+xlabel('r'); ylabel('|D_r|'); title('Magnitude Plot of A.2');
+subplot(2,1,2)
+stem(angle(X_r)) 
+xlabel('r'); ylabel('\angleD_r');title('Phase Plot of A.2');
+
+%% Problem 3
+clc
+N_0 = 6;                                                                % fundamental period of y[n]
+n = (0 : N_0-1);                                                        % fundamental frequency of y[n]
+Om_0 = 2*pi/N_0;
+
+y = [3 2 1 0 1 2];                                                      % definition of y[n]
+
+for r = 0:N_0-1,
+    Y_r(r+1) = sum(y.*exp(-j*r*n*Om_0))./N_0;                           % computing the DFTS of y[n]
 end
-r=n;
-X_r = fft(x)./N_1;
-figure;
-stem(r,abs(X_r),'k.');
-xlabel('r'); 
-ylabel('X_r');
-figure;
-stem(r,angle(X_r));
+
+figure
+subplot(2,1,1)
+stem(real(Y_r))
+xlabel('r'); ylabel('|D_r|'); title('Magnitude Plot of A.3');
+
+subplot(2,1,2)
+stem(angle(Y_r))
+xlabel('r'); ylabel('\angleD_r');title('Phase Plot of A.3');
+
 
 %% Part B Inverse DTFS
+clc
 N_2 = 32;
 n = (0:N_2-1);
 Omega_0 = (2.*pi)/N_2;
@@ -52,6 +60,7 @@ figure;
 stem(r,imag(X_r));
 
 %B.2
+clc
 N_2 = 32;
 n = (0:N_2-1);
 Omega_0 = (2.*pi)/N_2;
