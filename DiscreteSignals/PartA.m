b6ad4bbd5277ca7cdcf717200dc04cd1e863bbd5
Sameer Naumani <snaumani97@gmail.com>
2019-04-12 16:38:48 -0400

    lab 5

--- DiscreteSignals/PartA.m
+++ DiscreteSignals/PartA.m
@@ -1,4 +1,5 @@
 %% Lab5 PartA
+%1
 clc;
 N = 10;
 n = (0 : N-1);
@@ -13,6 +14,17 @@ subplot(2,1,1)
 stem(fr,abs(X1))
 subplot(2,1,2)
 stem(fr, abs(X2))
+
+%i
+% x2 has a symmetric spectrum because it is periodic and centered at 0.5
+%ii 
+% it is not possible to see the frequency components in x1 because the
+% exponentials are non periodi. In x2 you are able to see it because it is
+% periodic.
+% iii 
+% There are other frequency components because it is aperiodic
+
+
 
 %2
 clc;
@@ -32,6 +44,8 @@ stem(fft(abs(X_1)));
 subplot(2,1,2);
 stem(fft(abs(X_2)));
 
+% There is an improvement as you see the shape more clearly.
+
 %3
 clc;
 N = 100;
@@ -47,6 +61,11 @@ subplot(2,1,1)
 stem(fr,abs(X1))
 subplot(2,1,2)
 stem(fr, abs(X2))
+
+%They both seem to be symmetric because they are periodic with the
+%addition of the extra zeros. Now one of the zeros acts as the center
+%frequency. There are more samples.
+
 
 %4
 N = 500;
@@ -64,6 +83,9 @@ subplot(2,1,1);
 stem(fft(abs(X_1)));
 subplot(2,1,2);
 stem(fft(abs(X_2)));
+
+%There is an improvement because the general shape of the signal is more
+%visible.
 
 
 
